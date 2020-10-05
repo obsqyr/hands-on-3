ffc9a5ea93b007e6c44b6b743d30a6b71ef75414
nicse725 <nicse725@student.liu.se>
2020-10-05 15:39:08 +0200

    fixed inttest

--- inttest_md.py
+++ inttest_md.py
@@ -1,9 +1,11 @@
 import md
 import sys
+import os
 
 md.run_md()
 
-if 'cu.traj':
+if os.path.exists('cu.traj'):
     print("cu.traj exits")
 else:
     print("cu.traj does not exist")
+    sys.exit(1)
